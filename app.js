77d552e1adc7e723b3405f3d439f1d4aa529a354
zhangyanyan20 <zhangyanyan3@jd.com>
2021-04-23 11:15:04 +0800

    添加 redux

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import reducer from '../reducer/rootReducer';
+import reducer from './reducer/rootReducer';
 
-import Index from './index';
+import Index from './src/index';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
