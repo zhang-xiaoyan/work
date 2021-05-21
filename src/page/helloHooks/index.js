560a9edb00997ec3f20c10228111943dba2cb093
zhangyanyan <zhangyanyan@zhihu.com>
2021-05-21 20:41:18 +0800

    :chore 添加多个state

--- src/page/helloHooks/index.js
+++ src/page/helloHooks/index.js
@@ -1,15 +1,21 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 function HelloHooks(props){
     const [text, setText] = useState('初始文本');
-    function changeText(){
+    const [list, setList] = useState([]);
+    function changeText() {
         return setText('修改后的文本');
+    }
+    function changeList() {
+        return setList([1,2,3]);
     }
     return(
         <div>
-            <p>{text}</p>
+            <p>文案:{text}</p>
+            <p>长度:{list.length}</p>
             <button onClick={changeText}>{props.user}点击修改文本</button>
+            <button onClick={changeList}>点击修改数组</button>
         </div>
     );
 }
